55a1e7151de82c9a65b6c2a18c422cabb3acb5ad
NTPU <NTPU@host.docker.internal>
2024-06-28 15:44:11 +0800

    20240628

---
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.*;
+
+@RestController
+public class OrderController {
+
+	@Autowired
+	OrderService orderService;
+
+	@PostMapping("/ecpayCheckout")
+	public String ecpayCheckout() {
+		String aioCheckOutALLForm = orderService.ecpayCheckout();
+
+		return aioCheckOutALLForm;
+	}
+}
---
+++ src/test/java/com/example/demo/EcPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
